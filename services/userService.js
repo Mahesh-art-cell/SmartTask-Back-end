12a360e1a25ad1a2bb845502589562425bc572d5
Mahesh Talluri <mtalluri563@gmail.com>
2025-06-27 13:05:19 +0530

    update

--- services/userService.js
+++ services/userService.js
@@ -1,48 +1,4 @@
 
-
-// const User = require("../models/User");
-
-// const getUserProfile = async (userId) => {
-//   return await User.findById(userId).select("-password");
-// };
-
-// const updateUserProfile = async (userId, updates) => {
-//   return await User.findByIdAndUpdate(userId, updates, { new: true }).select("-password");
-// };
-
-// const getAllUsersService = async () => {
-//   return await User.find({ role: "user" }).select("-password");
-// };
-
-// const deactivateUserService = async (userId) => {
-//   return await User.findByIdAndUpdate(userId, { isActive: false }, { new: true });
-// };
-
-// // ✅ Add this function
-// const activateUserService = async (userId) => {
-//   return await User.findByIdAndUpdate(userId, { isActive: true }, { new: true });
-// };
-
-// const getDashboardStatsService = async () => {
-//   const totalUsers = await User.countDocuments({ role: "user" });
-//   const activeUsers = await User.countDocuments({ role: "user", isActive: true });
-//   const deactivatedUsers = await User.countDocuments({ role: "user", isActive: false });
-
-//   return { totalUsers, activeUsers, deactivatedUsers };
-// };
-
-
-// module.exports = {
-//   getUserProfile,
-//   updateUserProfile,
-//   getAllUsersService,
-//   deactivateUserService,
-//   activateUserService, // ✅ Export it
-//   getDashboardStatsService,
-// };
-
-
-
 const User = require("../models/User");
 const Task = require("../models/Task"); // ✅ Import Task model
 const mongoose = require("mongoose");
