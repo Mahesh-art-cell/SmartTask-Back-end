d021ced8c9dbc07c2fc40eb5ae4b863a163d6064
Mahesh Talluri <mtalluri563@gmail.com>
2025-07-07 15:01:40 +0530

    added

--- models/Task.js
+++ models/Task.js
@@ -5,7 +5,8 @@ const taskSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, required: true },
-    description: { type: String },
+    description: { type: String, required: true },
+    category:{type:String,required: true},
     dueDate: { type: Date, required: true },
     status: { type: String, enum: ["pending", "completed"], default: "pending" },
   },
